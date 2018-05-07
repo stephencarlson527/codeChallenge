ad3110ee47bc32c6ab46c9789a07d9819f56a482
Stephen Carlson <scdesignlife@gmail.com>
2018-05-07 15:29:55 -0400

    clean up code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,49 +1,49 @@
-$( document ).ready(function() {
-	
-    		function wsAtc() {
-          $("#ws-atc").click(function() {
-	          $("#ws-modal, #ws-modal .modal").fadeIn(800, function() {});
-          });
-         
-          $("#ws-modal .ws-close, #ws-modal .ws-btn").click(function(event) { 
-            if($('#ws-modal').is(":visible")) {
-              $("#ws-modal, #ws-modal .modal").fadeOut(800, function() {});
-            }    
-          });
-    		}
-    		wsAtc();
+$(document).ready(function() {
 
-    		function productImages() {
-    			var thumbNails = document.querySelectorAll('#ws-imgThumbs li');
-    			var mainImgList = document.querySelectorAll('#ws-imgMain li');
-    			$(mainImgList).nextAll().hide();
-    			$(thumbNails).click(function() {
-    				var currentImg = $(this).index();
-    				if($(thumbNails).hasClass('sm-img-active')) {
-    					$(thumbNails).removeClass('sm-img-active');
-    					$(this).addClass('sm-img-active');
-    				}
-    				$(mainImgList).eq(currentImg).show().siblings().hide();
-    			});
-    		}
-    		productImages();
+    function wsAtc() {
+        $('#ws-atc').click(function() {
+            $('#ws-modal, #ws-modal .modal').fadeIn(800);
+        });
 
-    		function readMore() {
+        $('#ws-modal .ws-close, #ws-modal .ws-btn').click(function(event) {
+            if ($('#ws-modal').is(':visible')) {
+                $('#ws-modal, #ws-modal .modal').fadeOut(800);
+            }
+        });
+    }
+    wsAtc();
 
-    			if($('.ws-expanded').length) {
-    				$('.ws-expanded').find('.ws-content').show();
-    			}
-    			$('.ws-drawer').click(function() {
-    				$(this).find('.ws-content').slideToggle();
-    				var title = $(this).find('h3');
-    				if(title.text() == 'Collapsed') {
-    					$(title).text('Expanded');
-    					$(this).find('.ws-collapsed').attr("class", "ws-expanded");
-    				} else {
-    					$(title).text('Collapsed');
-    					$(this).find('.ws-expanded').attr("class", "ws-collapsed");
-    				}
-    			});
-    		}
-    		readMore();
+    function productImages() {
+        var thumbNails = document.querySelectorAll('#ws-imgThumbs li');
+        var mainImgList = document.querySelectorAll('#ws-imgMain li');
+        $(mainImgList).nextAll().hide();
+        $(thumbNails).click(function() {
+            var currentImg = $(this).index();
+            if ($(thumbNails).hasClass('sm-img-active')) {
+                $(thumbNails).removeClass('sm-img-active');
+                $(this).addClass('sm-img-active');
+            }
+            $(mainImgList).eq(currentImg).show().siblings().hide();
+        });
+    }
+    productImages();
+
+    function readMore() {
+
+        if ($('.ws-expanded').length) {
+            $('.ws-expanded').find('.ws-content').show();
+        }
+        $('.ws-drawer').click(function() {
+            $(this).find('.ws-content').slideToggle();
+            var title = $(this).find('h3');
+            if (title.text() == 'Collapsed') {
+                $(title).text('Expanded');
+                $(this).find('.ws-collapsed').attr('class', 'ws-expanded');
+            } else {
+                $(title).text('Collapsed');
+                $(this).find('.ws-expanded').attr('class', 'ws-collapsed');
+            }
+        });
+    }
+    readMore();
 });
